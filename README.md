c35584e971b0caa2c7474c6765de2d81963966f1
chemtrails <dev@cronguy.com>
2023-05-15 18:16:11 +0200

    Change default

--- README.md
+++ README.md
@@ -1,65 +1,25 @@
 ### Terval measures, formats and prints performance
 
 ```console
-pip install git+https://github.com/chemtrails/terval
+pip install git+https://github.com/chemtrails/terval.git
 ```
 
-Fake function:
+### Example
 
 ```py
-import time
-def fake():
-    time.sleep(0.1)
-```
-
-### Example of a default timer
-
-Call `x()` after every task
-
-Call `toggle()` to stop and resume printing
-
-```py
+from time import sleep
 from terval import Terval
 
-timer = Terval()
+t = Terval()
 
-fake()
-timer.x()
-
-# stop printing
-timer.toggle()
-
-fake()
-# this will not be printed
-timer.x()
-
-# resume printing
-timer.toggle()
-
-fake()
-timer.x()
-```
-
-![image](https://raw.githubusercontent.com/chemtrails/terval/master/images/terminal2.png)
-
-### Example of a silent timer and named task
-
-`print(timer)` to show summary
-
-```py
-timer = Terval("timer", silent=True)
-
-fake()
-timer.x("named task")
+sleep(0.1)
+t.x("named task")
 
 for _ in range(2):
-    fake()
-    timer.x()
+    sleep(0.1)
+    t.x()
 
 print(timer)
 ```
 
 ![image](https://raw.githubusercontent.com/chemtrails/terval/master/images/terminal.png)
-
-### Dependencies
-- tabulate
--- src/terval/__init__.py
+++ src/terval/__init__.py
@@ -3,7 +3,7 @@ from tabulate import tabulate
 
 
 class Terval:
-    def __init__(self, name: str = "TERVAL", silent: bool = False) -> None:
+    def __init__(self, name: str = "TERVAL", silent: bool = True) -> None:
         self.prev_time = self.first_time = perf_counter_ns()
         self.name = name
         self.silent = silent
@@ -11,14 +11,14 @@ class Terval:
         self.all = ()
 
     def __str__(self) -> str:
-        return tabulate(self.all + (("TOTAL", f"{(self.prev_time - self.first_time):_} ns"),), headers=(self.name, "PERFORMANCE"), tablefmt="fancy_grid")
+        return tabulate(self.all + (("TOTAL", f"{(self.prev_time - self.first_time):_}"),), headers=(self.name, "PERFORMANCE (ns)"), tablefmt="fancy_grid")
 
     def x(self, name: str = "") -> None:
-        elapsed = f"{((t := perf_counter_ns()) - self.prev_time):_} ns"
+        elapsed = f"{((t := perf_counter_ns()) - self.prev_time):_}"
         if not name:
             name = f"TASK {self.count}"
-        if not self.silent:
-            print(tabulate(((self.name, name, elapsed),), tablefmt="fancy_grid"))
+        if self.silent == False:
+            print(tabulate(((self.name, name, f"{elapsed} ns"),), tablefmt="fancy_grid"))
         self.prev_time = t
         self.count += 1
         self.all += ((name, elapsed),)
